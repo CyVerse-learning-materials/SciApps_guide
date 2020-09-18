e42c199287585cc28c5b8c35b09dbe6714605957
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2020-09-18 10:43:52 -0400

    add triage issue template

--- conf.py
+++ conf.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Version 2.0, May 2020
 # -*- coding: utf-8 -*-
 #
 # General information about the project.
@@ -9,15 +10,13 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'SciApps Guide'
-copyright = '2017, CyVerse'
-author = 'CyVerse'
-version = '1.0'
-release = '1.0'
+project = 'YOUR GUIDE NAME HERE'
+copyright = '2020, CyVerse'
+author = 'your_name'
+version = '2.0'
+release = '2.0'
 
-html_static_path = []
 epub_title = project
 epub_author = author
 epub_publisher = author
 epub_copyright = copyright
-exclude_patterns = ['README.md', 'License.md', 'Contributors_maintainers.md']
